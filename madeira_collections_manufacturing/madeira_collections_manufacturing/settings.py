3627d0e46a79f9839e15a23db998d3b939c6ceab
vjveeranandhan <vjveeranandhan@gmail.com>
2025-01-15 17:48:18 +0200

    database changed and updated

--- madeira_collections_manufacturing/madeira_collections_manufacturing/settings.py
+++ madeira_collections_manufacturing/madeira_collections_manufacturing/settings.py
@@ -89,14 +89,21 @@ WSGI_APPLICATION = 'madeira_collections_manufacturing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'madeira',      # Replace with your database name
+#         'USER': 'veeran',          # Replace with your database username
+#         'PASSWORD': 'veeran',  # Replace with your database password
+#         'HOST': '127.0.0.1',       # Replace with your database host (e.g., 'localhost')
+#         'PORT': '5432',            # Default PostgreSQL port
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'madeira',      # Replace with your database name
-        'USER': 'veeran',          # Replace with your database username
-        'PASSWORD': 'veeran',  # Replace with your database password
-        'HOST': '127.0.0.1',       # Replace with your database host (e.g., 'localhost')
-        'PORT': '5432',            # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',  # SQLite engine
+        'NAME': BASE_DIR / 'db.sqlite3',  # Path to the SQLite database file
     }
 }
 
